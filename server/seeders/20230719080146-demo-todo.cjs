9c0afc99cf6b50515859555538494d09eef7a72e
Stepan <takistepan@gmail.com>
2023-07-19 11:11:41 +0300

    Добавлены данные для заполнения базы данных
    Добавлены данные для заполнения таблицы Todos

---
+++ server/seeders/20230719080146-demo-todo.cjs
@@ -0,0 +1,48 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Todos", [
+      {
+        content: "throw out the trash",
+        category: "personal",
+        done: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        content: "feed the dog",
+        category: "personal",
+        done: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        content: "buy groceries at home",
+        category: "business",
+        done: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        content: "check email",
+        category: "personal",
+        done: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        content: "pick up delivery",
+        category: "personal",
+        done: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Todos", null, {});
+  },
+};
